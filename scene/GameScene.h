a61a3fcd3278cfedaba8b6a1a787ef07c9f5407a
Kazukiii <k021g1313@g.neec.ac.jp>
2022-06-06 14:00:08 +0900

    01_02_確認課題

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -51,4 +51,28 @@ class GameScene {
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
+	
+	// スプライト
+	Sprite* sprite_ = nullptr;
+
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+
+	//3Dモデル
+	Model* model_ = nullptr;
+
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_;
+
+	//ビュープロジェクション
+	ViewProjection viewProjection_;
+
+	//サウンドデータハンドル
+	uint32_t soundDateHandle_ = 0;
+
+	//音声再生ハンドル
+	uint32_t voiceHandle_ = 0;
+
+	//値を表示したい変数
+	int32_t value_ = 0;
 };
